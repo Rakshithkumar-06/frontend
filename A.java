1386239fcc1a48fc42fb9940b50bb8ce2af79260
H E Rakshith kumar <rakshithkumar6dec@gmail.com>
2023-05-31 18:44:32 +0530

    first task in html

---
+++ A.java
@@ -0,0 +1,21 @@
+class A{
+	int rollno;
+	String name;
+	static String college="CUS";
+	A(int r,String n){
+		rollno=r;
+		name=n;
+	}
+	void display(){
+		System.out.println(rollno+" "+name+" "+college);
+		}
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+A s1=new A(111,"rakshi");
+A s2=new A(222,"siva");
+s1.display();
+s2.display();
+	}
+
+}
